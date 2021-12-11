f3afaeeb6d1cd144f59a6e9680e9cb1fe015431f
Europa_You <v1europayou@gmail.com>
2021-12-11 20:20:43 +0300

    Added decorative_blast_furnace and decorative_furnace

---
+++ src/main/java/com/europayou/buildingblocks/vertical_blocks/vertical_spruce_plank_slab.java
@@ -0,0 +1,48 @@
+package com.europayou.buildingblocks.vertical_blocks;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.*;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+public class vertical_spruce_plank_slab extends HorizontalFacingBlock {
+
+    public vertical_spruce_plank_slab() {
+		super(FabricBlockSettings.of(Material.WOOL).breakByTool(FabricToolTags.PICKAXES).sounds(BlockSoundGroup.WOOD).strength(2, 0.2f));
+		setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+		stateManager.add(Properties.HORIZONTAL_FACING);
+	}
+
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext ctx) {
+		Direction dir = state.get(FACING);
+		switch(dir) {
+			case NORTH:
+				return VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.5f);
+			case SOUTH:
+				return VoxelShapes.cuboid(0.0f, 0.0f, 0.5f, 1.0f, 1.0f, 1.0f);
+			case EAST:
+				return VoxelShapes.cuboid(0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+			case WEST:
+				return VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 0.5f, 1.0f, 1.0f);
+			default:
+				return VoxelShapes.fullCube();
+		}
+	}
+
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		return (BlockState)this.getDefaultState().with(FACING, ctx.getPlayerFacing());
+    }
+}
